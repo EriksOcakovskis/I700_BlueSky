cd3a353d2b9dc84ad8157f3cabe1bd46c3eff1d2
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2016-10-19 09:41:59 +0300

    Add very simple rain effect
    A very simple implementation of rain, it is not optimised.

--- core/src/game/BlueSky.java
+++ core/src/game/BlueSky.java
@@ -6,8 +6,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class BlueSky extends ApplicationAdapter {
     private SpriteBatch batch;
@@ -15,19 +20,22 @@ public class BlueSky extends ApplicationAdapter {
     private Texture idleStanceImage1;
     private Texture idleStanceImage2;
     private Texture backgroundImage;
+    private Texture dropletImage;
     private Music backgroundMusic;
     private Rectangle player;
+    private Array<Rectangle> raindrops;
+    private long lastDropTime;
 
     public void movePlayerRight(int button, int altButton) {
         if (Gdx.input.isKeyPressed(button) || Gdx.input.isKeyPressed(altButton)) {
-            player.x += 200 * Gdx.graphics.getDeltaTime();
+            player.x += 300 * Gdx.graphics.getDeltaTime();
             playerBoundaries();
         }
     }
 
     public void movePlayerLeft(int button, int altButton) {
         if (Gdx.input.isKeyPressed(button) || Gdx.input.isKeyPressed(altButton)) {
-            player.x -= 200 * Gdx.graphics.getDeltaTime();
+            player.x -= 300 * Gdx.graphics.getDeltaTime();
             playerBoundaries();
         }
     }
@@ -37,6 +45,16 @@ public class BlueSky extends ApplicationAdapter {
         if(player.x > Gdx.graphics.getWidth() - player.getWidth()) {
             player.x = Gdx.graphics.getWidth() - player.getWidth();
         }
+    }
+
+    public void makeItRain(){
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, Gdx.graphics.getWidth() - 7);
+        raindrop.y = Gdx.graphics.getHeight();
+        raindrop.width = 7;
+        raindrop.height = 11;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
     }
 
 
@@ -50,13 +68,19 @@ public class BlueSky extends ApplicationAdapter {
         backgroundImage = new Texture(Gdx.files.internal("backdrop.png"));
         backgroundImage.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
 
+        dropletImage = new Texture(Gdx.files.internal("rain.png"));
+
         idleStanceImage1 = new Texture(Gdx.files.internal("stance1_1.png"));
-        idleStanceImage2 = new Texture(Gdx.files.internal("stance2_1.png"));
+        // idleStanceImage2 = new Texture(Gdx.files.internal("stance2_1.png"));
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("undertreeinrain.mp3"));
 
         // Loop the background music from beginning
         backgroundMusic.setLooping(true);
         backgroundMusic.play();
+
+        // Create rain
+        raindrops = new Array<Rectangle>();
+        makeItRain();
 
         // Creating player
         player = new Rectangle();
@@ -72,6 +96,19 @@ public class BlueSky extends ApplicationAdapter {
         Gdx.gl.glClearColor(1, 1, 1, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Spawn a raindrop every 0.01 second
+        if (TimeUtils.nanoTime() - lastDropTime > 10000000) {
+            makeItRain();
+        }
+
+        // Make raindrop move 800px per second
+        Iterator<Rectangle> iter = raindrops.iterator();
+        while(iter.hasNext()) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 800 * Gdx.graphics.getDeltaTime();
+            if(raindrop.y + 11 < 0) iter.remove();
+        }
+
         // Update camera once per frame
         camera.update();
 
@@ -81,6 +118,9 @@ public class BlueSky extends ApplicationAdapter {
         batch.begin();
         batch.draw(backgroundImage, 0, 0 , Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(idleStanceImage1, player.getX(), player.getY(), player.getWidth(), player.getHeight());
+        for(Rectangle raindrop: raindrops) {
+            batch.draw(dropletImage, raindrop.x, raindrop.y);
+        }
         batch.end();
 
         // Player movement
@@ -95,5 +135,6 @@ public class BlueSky extends ApplicationAdapter {
         idleStanceImage1.dispose();
         backgroundImage.dispose();
         backgroundMusic.dispose();
+        dropletImage.dispose();
     }
 }
