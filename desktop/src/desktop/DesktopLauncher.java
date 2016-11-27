d97dd139423ce2ad59b00782bbd7d3ebad9e8021
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2016-11-27 14:25:50 +0200

    Correct build parameters
    Gradle builds now will have correct app name, version, timestamp and
    main location

--- desktop/src/desktop/DesktopLauncher.java
+++ desktop/src/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import game.BlueSky;
 public class DesktopLauncher {
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.fullscreen = false;
+        config.fullscreen = true;
         config.title = "Blue Sky";
         config.width = 1440;
         config.height = 900;
